b10d1deaaa3f4d0637604143c20d516c15ec9c6a
MiszukUwU <aleksanderowsinski23@gmail.com>
2024-10-24 21:58:28 +0200

    Documentation

--- Assets/Script/Player/MovementController.cs
+++ Assets/Script/Player/MovementController.cs
@@ -7,8 +7,8 @@ using UnityEngine;
 [SelectionBase]
 public class MovementController : MonoBehaviour
 {
-    
     [SerializeField] private float movementSpeed = 5.0f;
+    
     private Vector2 movementInput;
     private Rigidbody2D playerRb;
     
@@ -16,20 +16,22 @@ public class MovementController : MonoBehaviour
     {
         playerRb = GetComponent<Rigidbody2D>();
     }
-    
+
+    /// <summary>
+    /// Gets the player's movement input from the Input system.
+    /// </summary>
     void getInput()
     {
-        movementInput = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));    
+        movementInput = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));
     }
     
     void Update()
     {
         getInput();
     }
-
-    private void FixedUpdate() 
+    
+    private void FixedUpdate()
     {
         playerRb.velocity = movementInput.normalized * (movementSpeed * Time.fixedDeltaTime);
     }
-    
 }
