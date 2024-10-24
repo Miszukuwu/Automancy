8cc1dabce045940c0a3ce40a49bae9f4e36d08f9
MiszukUwU <aleksanderowsinski23@gmail.com>
2024-10-24 21:58:28 +0200

    Documentation

--- Assets/Script/Interactable.cs
+++ Assets/Script/Interactable.cs
@@ -1,7 +1,13 @@
 namespace Script
 {
+    /// <summary>
+    /// Interface for objects that can be interacted with.
+    /// </summary>
     public interface Interactable
     {
+        /// <summary>
+        /// Method to be called when the object is interacted with.
+        /// </summary>
         void Interact();
     }
 }
--- Assets/Script/Player/InteractionController.cs
+++ Assets/Script/Player/InteractionController.cs
@@ -1,33 +1,42 @@
 using UnityEngine;
+using UnityEngine.Serialization;
 
 namespace Script.Player
 {
     public class InteractionController : MonoBehaviour 
     {
-        [SerializeField] private float reach = 1.0f;
+        [SerializeField] private float interactionReach = 1.0f;
         
-        private bool interactButton;
+        private bool isInteractPressed;
         
+        /// <summary>
+        /// Gets the player's input for interaction.
+        /// </summary>
         void getInput()
         {
-            interactButton = Input.GetButtonDown("Interact");
+            isInteractPressed = Input.GetButtonDown("Interact");
         }
         
         private void Update()
         {
             getInput();
-            if (interactButton) {
+            if (isInteractPressed) {
                 Debug.Log("Interact button pressed");
                 Interact();
             }
         }
 
+        /// <summary>
+        /// Shoots a raycast to check for interactable objects and calls their Interact method.
+        /// Currently only checks for objects to the right of the player.
+        /// </summary>
         private void Interact()
         {
             int layerMask = LayerMask.GetMask("Interactable");
-            RaycastHit2D hit = Physics2D.Raycast(transform.position, Vector2.right, reach, layerMask);
+            RaycastHit2D hit = Physics2D.Raycast(transform.position, Vector2.right, interactionReach, layerMask);
+            
             if (hit.collider == null) {
-                Debug.Log("Didnt hit anything");
+                Debug.Log("Didn't hit anything");
                 return;
             }
             
@@ -36,8 +45,8 @@ namespace Script.Player
                 Debug.Log("Not interactable");
                 return;
             }
+            
             interactable.Interact();
-
         }
     }
 }
