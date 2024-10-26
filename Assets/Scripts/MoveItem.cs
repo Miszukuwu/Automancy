506fb0283a92d2ee9946459967cd01a17566ca78
compactrex66 <compactrex66@gmail.com>
2024-10-26 15:11:59 +0200

    Improved item movement

--- Assets/Scripts/MoveItem.cs
+++ Assets/Scripts/MoveItem.cs
@@ -1,39 +1,18 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.AI.Navigation;
 using UnityEngine;
+using UnityEngine.AI;
 
 public class MoveItem : MonoBehaviour
 {
-    private GameObject nearestTarget;
-    private GameObject[] possibleTargets;
-    [SerializeField] private float movingSpeed = 100;
-
-    /// <summary>
-    /// Finds nearest game object from array to the game object
-    /// </summary>
-    /// <param name="possibleTargets">Array of GameObjects that may be targets</param>
-    /// <param name="gameObject">Game object that we check from</param>
-    /// <returns>Nearest game object</returns>
-    private GameObject FindTheNearestGameObject(GameObject[] possibleTargets, GameObject gameObject) {
-        GameObject nearestTarget;
-        nearestTarget = possibleTargets[0];
-
-        for(int i = 1; i < possibleTargets.Length; i++) {
-            if(Vector2.Distance(transform.position, possibleTargets[i].transform.position) < Vector2.Distance(transform.position, nearestTarget.transform.position)) {
-                nearestTarget = possibleTargets[i];
-            }
-        }
-
-        return nearestTarget;
-    }
+    
     void Start()
     {
-        possibleTargets = GameObject.FindGameObjectsWithTag("ItemTargetDestination");
-        nearestTarget = FindTheNearestGameObject(possibleTargets, gameObject);
+        GetComponent<NavMeshAgent>().destination = GameObject.FindGameObjectWithTag("Stash").transform.position;
     }
     void Update()
     {
-        //Moves item to the nearest target according to speed
-        transform.position += movingSpeed * Time.deltaTime * (nearestTarget.transform.position - transform.position).normalized;
+        transform.rotation = new Quaternion(0f, 0f, 0f, 0f);
     }
 }
