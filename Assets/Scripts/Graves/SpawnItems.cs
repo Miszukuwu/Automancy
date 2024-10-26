832d1fba9b38e94af881f74cbc53c400ccc70bb6
compactrex66 <compactrex66@gmail.com>
2024-10-26 15:11:59 +0200

    Improved item movement

--- Assets/Scripts/Graves/SpawnItems.cs
+++ Assets/Scripts/Graves/SpawnItems.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -6,16 +7,22 @@ public class SpawnItems : MonoBehaviour
 {
 
     private float timer = 0;
+    private GameObject itemGroup;
     [SerializeField] private float timeBetweenItemSpawn = 5;
     [SerializeField] private GameObject item;
 
-    // Update is called once per frame
+    private void Start()
+    {
+        if (GameObject.Find("Items"))
+            itemGroup = GameObject.Find("Items");
+    }
+
     void Update()
     {
         timer += Time.deltaTime;
 
         if(timer >= timeBetweenItemSpawn) {
-            GameObject newItem = Instantiate(item, transform.position, transform.rotation);
+            GameObject newItem = Instantiate(item, transform.position, transform.rotation, itemGroup.transform);
             timer = 0;
         }
     }
