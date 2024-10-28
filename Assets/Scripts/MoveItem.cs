e6506d615ddbd0d4d60ef0dc917ed642d176ba42
compactrex66 <compactrex66@gmail.com>
2024-10-28 14:52:26 +0100

    Reapply "Added item assembler"
    This reverts commit b488b4661adcd2ddacc9368e4bd18f21f4992f6a.

--- Assets/Scripts/MoveItem.cs
+++ Assets/Scripts/MoveItem.cs
@@ -6,13 +6,23 @@ using UnityEngine.AI;
 
 public class MoveItem : MonoBehaviour
 {
-    
+    private NavMeshAgent agent;
+    private GameObject targetObject;
+    private ZombieAssembler zombieAssemblerScript;
     void Start()
     {
-        GetComponent<NavMeshAgent>().destination = GameObject.FindGameObjectWithTag("Stash").transform.position;
+        agent = GetComponent<NavMeshAgent>();
+        targetObject = GameObject.FindGameObjectWithTag("Stash");
+        agent.destination = targetObject.transform.position;
+        zombieAssemblerScript = targetObject.GetComponent<ZombieAssembler>();
     }
     void Update()
     {
         transform.rotation = new Quaternion(0f, 0f, 0f, 0f);
+        if (agent.remainingDistance <= 0.1 && zombieAssemblerScript.amountOfItems < 5)
+        {
+            zombieAssemblerScript.amountOfItems++;
+            Destroy(gameObject);
+        }
     }
 }
