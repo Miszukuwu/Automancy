29463c5ad48b68f17c0b969d0cf3220c6af5fd8f
compactrex66 <compactrex66@gmail.com>
2024-10-30 22:03:46 +0100

    It's now impossible to get negative money

--- Assets/Scripts/Graves/GraveInteraction.cs
+++ Assets/Scripts/Graves/GraveInteraction.cs
@@ -12,6 +12,28 @@ public class GraveInteraction : MonoBehaviour, Interactable
     {
         maxGraveLevel = GameManager.graveSprites.Length - 1;
         
+        switch (graveLevel+1)
+        {
+            case 1:
+                if (GameManager.playerMoney - 75 < 0)
+                {
+                    return;
+                }
+                GameManager.playerMoney -= 75; break;
+            case 2:
+                if (GameManager.playerMoney - 200 < 0)
+                {
+                    return;
+                }
+                GameManager.playerMoney -= 200; break;
+            case 3:
+                if (GameManager.playerMoney - 500 < 0)
+                {
+                    return;
+                }
+                GameManager.playerMoney -= 500; break;
+        }
+        
         graveLevel++;
         if (graveLevel <= maxGraveLevel)
         {
@@ -21,13 +43,6 @@ public class GraveInteraction : MonoBehaviour, Interactable
         if (graveLevel != 1 && graveLevel <= maxGraveLevel)
         {
             spawnItemsScript.timeBetweenItemSpawn -= 2;
-        }
-
-        switch (graveLevel)
-        {
-            case 1: GameManager.playerMoney -= 75; break;
-            case 2: GameManager.playerMoney -= 200; break;
-            case 3: GameManager.playerMoney -= 500; break;
         }
 
         Debug.Log(GameManager.playerMoney);
