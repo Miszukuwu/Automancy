29c835671b2bc9f3899d2affe77b1aa9b25ce126
compactrex66 <compactrex66@gmail.com>
2024-10-30 17:29:53 +0100

    Added market stall script

---
+++ Assets/Scripts/MarketStall.cs
@@ -0,0 +1,6 @@
+using UnityEngine;
+
+public class MarketStall : MonoBehaviour
+{
+    [HideInInspector] public int amountOfStashedItems = 0;
+}
--- Assets/Scripts/MoveItem.cs
+++ Assets/Scripts/MoveItem.cs
@@ -8,6 +8,8 @@ public class MoveItem : MonoBehaviour
 {
     private NavMeshAgent agent;
     private GameObject targetObject;
+    private MarketStall marketStallScript;
+    [SerializeField] private int itemPrice = 10;
     private ZombieAssembler zombieAssemblerScript;
     private Sprite[] itemSprites;
     void Start()
@@ -15,6 +17,7 @@ public class MoveItem : MonoBehaviour
         agent = GetComponent<NavMeshAgent>();
         targetObject = GameObject.FindGameObjectWithTag("Stash");
         agent.destination = targetObject.transform.position;
+        marketStallScript = targetObject.GetComponent<MarketStall>();
         
         itemSprites = Resources.LoadAll<Sprite>("Items");
         transform.GetChild(0).GetComponent<SpriteRenderer>().sprite = itemSprites[UnityEngine.Random.Range(0, itemSprites.Length)];
@@ -24,6 +27,7 @@ public class MoveItem : MonoBehaviour
         transform.rotation = new Quaternion(0f, 0f, 0f, 0f);
         if (agent.remainingDistance <= 0.1)
         {
+            marketStallScript.amountOfStashedItems++;
             Destroy(gameObject);
         }
     }
